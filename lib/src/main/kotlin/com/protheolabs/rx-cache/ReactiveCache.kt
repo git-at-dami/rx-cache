f1b406272b4b5aeaafdafa727490665842c74375
git-at-dami <dami.func@gmail.com>
2025-04-06 14:59:29 +0000

    reactive cache skeleton
    - cache update subscription
    - store active subscriber in concurrent hash map and notify subscriber

---
+++ lib/src/main/kotlin/com/protheolabs/rx-cache/ReactiveCache.kt
@@ -0,0 +1,50 @@
+import org.reactivestreams.Publisher
+import org.reactivestreams.Subscriber
+import org.reactivestreams.Subscription
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicLong
+
+/**
+ * in-memory reactive cache that emits updates through the Reactive Streams API.
+ */
+class ReactiveCache<K, V> : Publisher<CacheUpdate<K, V>> {
+    /**
+     * A map to store the currently active subscribers and their subscriptions for update events.
+     */
+    private val updateSubscribers = ConcurrentHashMap<Subscriber<in CacheUpdate<K, V>>, CacheUpdateSubscription<K, V>>()
+
+    /**
+     * Subscribes the given [Subscriber] to receive [CacheUpdate] events from this cache.
+     * @param s The subscriber that wants to receive updates.
+     */
+    override fun subscribe(s: Subscriber<in CacheUpdate<K, V>>) {
+        // Create a new subscription for this subscriber.
+        val subscription = CacheUpdateSubscription(s, this)
+        // Store the subscription.
+        updateSubscribers[s] = subscription
+        // Notify the subscriber that the subscription has been established.
+        s.onSubscribe(subscription)
+    }
+
+    /**
+     * Represents the subscription between a Publisher (ReactiveCache) and a Subscriber for CacheUpdate events.
+     */
+    internal class CacheUpdateSubscription<K, V>(
+        private val subscriber: Subscriber<in CacheUpdate<K, V>>,
+        private val publisher: ReactiveCache<K, V>
+    ) : Subscription {
+        /**
+         * Requests a specific number of additional items from the Publisher.
+         * @param n The number of items to request. Must be positive.
+         */
+        override fun request(n: Long) {
+        }
+
+        /**
+         * Cancels the subscription. The Publisher will eventually stop sending more items.
+         */
+        override fun cancel() {
+        }
+    }
+}
