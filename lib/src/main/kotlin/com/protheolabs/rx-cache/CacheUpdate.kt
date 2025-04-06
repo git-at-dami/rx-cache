6b1d08f5f152acb2d4e8cc7628f6302ba6adf672
git-at-dami <dami.func@gmail.com>
2025-04-06 14:53:44 +0000

    add sealed data class
    representing an update event in the cache (put/delete)

---
+++ lib/src/main/kotlin/com/protheolabs/rx-cache/CacheUpdate.kt
@@ -0,0 +1,17 @@
+/**
+ * Represents an update operation on the cache.
+ */
+ sealed class CacheUpdate<K, V> {
+    /**
+     * Represents storing a key-value item pair into the cache.
+     * @param key The key of the item
+     * @param value The value being associated with the key.
+     */
+    data class Put<K, V>(val key: K, val value: V) : CacheUpdate<K, V>()
+
+        /**
+     * Represents removing a key from the cache.
+     * @param key The key being removed.
+     */
+    data class Delete<K, V>(val key: K) : CacheUpdate<K, V>()
+ }
